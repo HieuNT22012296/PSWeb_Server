008022c6a8b04f479208aef5680ecfd2e6068421
Cung Thịnh <macthinh22@gmail.com>
2024-05-21 10:16:20 +0700

    Thêm login với registation API

---
+++ Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PSWeb_Server.Models;
+using Microsoft.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PSWeb_Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public Response register(Users users)
+        {
+            Response response = new Response();
+            DataAccessLayer dal = new DataAccessLayer();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("EMedCs").ToString());
+            response = dal.register(users, connection);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public Response Login(Users users)
+        {
+            Response response = new Response();
+            DataAccessLayer dal = new DataAccessLayer();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("EMedCs").ToString());
+            response = dal.Login(users, connection);
+            return response;
+        }
+    }
+}
+
---
+++ Models/DataAccessLayer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+namespace PSWeb_Server.Models
+{
+	// Các database logic để ở đây
+	public class DataAccessLayer
+	{
+		public Response register(Users users, SqlConnection connection)
+		{
+			Response response = new Response();
+			SqlCommand cmd = new SqlCommand("sp_register", connection);
+			cmd.CommandType = System.Data.CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@FirstName", users.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", users.LastName);
+            cmd.Parameters.AddWithValue("@Password", users.Password);
+            cmd.Parameters.AddWithValue("@Email", users.Email);
+            cmd.Parameters.AddWithValue("@Fund", 0);
+            cmd.Parameters.AddWithValue("@Type", "Users");
+			cmd.Parameters.AddWithValue("@Type", "Pending");
+			connection.Open();
+			int i = cmd.ExecuteNonQuery();
+			connection.Close();
+			if(i > 0)
+			{
+				response.StatusCode = 200;
+				response.StatusMessage = "User registed successfully";
+			}
+			else
+			{
+				response.StatusCode = 100;
+				response.StatusMessage = "User registation failed";
+			}
+			return response;
+        }
+
+		public Response Login(Users users, SqlConnection connection)
+		{
+			SqlDataAdapter da = new SqlDataAdapter("sp_login", connection);
+			da.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+			da.SelectCommand.Parameters.AddWithValue("@Email", users.Email);
+            da.SelectCommand.Parameters.AddWithValue("@Password", users.Password);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			Response response = new Response();
+			if(dt.Rows.Count > 0)
+			{
+				response.StatusCode = 200;
+				response.StatusMessage = "User is valid";
+			}
+			else
+			{
+                response.StatusCode = 100;
+                response.StatusMessage = "User is invalid";
+            }
+            return response;
+		}
+	}
+}
+
